cc20347b82d231ada3e047ca1ac1af8f8c49d836
Rahul Bhurtel <136094611+Rahcurl@users.noreply.github.com>
2025-08-23 06:37:30 +0000

    comparision mainly null and undefined with echma js

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,27 @@
+console.log(2>1)
+console.log(2>=1)
+console.log(2<1)
+console.log(2 == 1)
+console.log(2!=1)
+
+console.log("string")
+
+//typescript doesn't allow different dataTypes in comparision
+//for eg ,
+console.log("2" > 1) //true
+console.log("02" > 1) //true
+
+console.log("null")
+
+console.log(null > 0) //false
+console.log(null == 0) //false
+console.log(null >= 0) //true
+
+
+//undefined will give the value false
+console.log("undefined")
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
+
+
