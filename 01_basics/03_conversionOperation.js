2a68d4a538564a88ab0b557ca2b8b5f721e918bd
Rahul Bhurtel <136094611+Rahcurl@users.noreply.github.com>
2025-08-23 06:36:48 +0000

    conversionOperation in prefix and postfix

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -36,3 +36,50 @@ let someNumbers = null
 let stringValue = String(someNumbers)
 console.log(stringValue)
 console.log(typeof stringValue)
+
+let value = 3
+let negValue  = -value
+console.log(negValue)
+
+console.log(2+3)
+console.log(2**2)
+console.log(2/3)
+console.log(2%3)
+
+//adding strings
+
+let str1 = "hello"
+let str2 = " Rahul" //space infront of name
+//add or concatenate
+let str3 = str1 + str2
+console.log(str3)
+
+//priority
+console.log("1" + 2) // output 12
+console.log(1 + "2") //output 12
+console.log("1" + 2 + 2) //you are thinking that there will be 14 in output but output --> 122
+console.log(1 + 2 + "2") //output 32
+
+console.log((3 + 3) * 4 % 5) //use parenthesis as much you can in this type of operation
+
+/* 
+console.log(+true) //will fetch true
+console.log(true+) //error
+*/
+
+
+let num1, num2, num3
+num1 = num2 = num3 =(2+2)
+
+let gameCounter = 100
+gameCounter++;
+
+console.log(gameCounter)
+
+//lets dive into prefix and postfix increment
+let x = 3;
+const y = x++; // y gets the OLD value of x (3), then x becomes 4
+// output y = 3  and x = 4
+let a = 3;
+const b = ++a; // a is incremented to 4 first, then b gets that value
+//output a = 4 and  b = 4
