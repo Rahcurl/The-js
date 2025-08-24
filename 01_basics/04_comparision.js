d945144689edc2b144267f64d9a0e49ce1a9f14f
Rahul Bhurtel <136094611+Rahcurl@users.noreply.github.com>
2025-08-24 10:59:40 +0000

    comparision maily null and undefined using strict assignment operator

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -23,5 +23,13 @@ console.log("undefined")
 console.log(undefined == 0)
 console.log(undefined > 0)
 console.log(undefined < 0)
+/* 
+the reason is thatan equility check == and comparision > <>= <= work differently .
+
+comparisions converts null to a number ,treating it as 0.
+that's why
+3. null >= 0 is true and,
+1.null > 0 is false 
+*/
 
 
